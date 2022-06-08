9731c9ac47366930bcaebbe368a0e48d75720d10
Evija M <85995321+Evija-M@users.noreply.github.com>
2022-06-08 14:31:31 +0300

    Update new_article_flow_spec.rb

--- spec/requests/new_article_flow_spec.rb
+++ spec/requests/new_article_flow_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe 'New article', type: :request do
       fill_in 'Password', with: user.password
       click_button 'Log in'
 
-      get new_article_path
+      visit new_article_path
       expect(response).to have_http_status(200)
     end
   end
